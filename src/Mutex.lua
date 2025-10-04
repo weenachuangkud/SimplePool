c312fa3b7277624ceeb9ace23c39cf578e023438
Mawin <weena.chuangkud@gmail.com>
2025-10-04 18:07:14 +0700

    Create Mutex.lua
    MAMAMAMAMAA

---
+++ src/Mutex.lua
@@ -0,0 +1,67 @@
+--[[
+Modded by Mawin CK
+- Date : Saturday, October 4, 2568 BE
+- Description : A simple mutex class for synchronizing access to shared resources.
+]]
+
+-- very simple mutex implementation :D
+
+-- Services
+local RunService = game:GetService("RunService")
+
+-- Def
+
+export type Mutex = {
+	locked: boolean,
+	lockingThread: thread?,
+	lockCount: number,
+	lock: (self: Mutex, timeout: number?) -> (),
+	unlock: (self: Mutex) -> (),
+	isLocked: (self: Mutex) -> boolean
+}
+
+-- Mutex
+
+local Mutex = {}
+Mutex.__index = Mutex
+
+function Mutex.new()
+	local self = (setmetatable({}, Mutex) :: any) :: Mutex
+	self.locked = false
+	self.lockingThread = nil
+	self.lockCount = 0
+	return self
+end
+
+function Mutex:lock(timeout)
+	local startTime = tick()
+	local currentThread = coroutine.running()
+
+	while self.locked and self.lockingThread ~= currentThread do
+		if timeout and (tick() - startTime) >= timeout then
+			error("Mutex lock timeout")
+		end
+		RunService.Heartbeat:Wait()
+	end
+	self.locked = true
+	self.lockingThread = currentThread
+	self.lockCount += 1
+end
+
+function Mutex:unlock()
+	local currentThread = coroutine.running()
+	if not self.locked or self.lockingThread ~= currentThread then
+		error("Mutex unlock attempt by non-locking thread or when mutex is not locked")
+	end
+	self.lockCount -= 1
+	if self.lockCount == 0 then
+		self.locked = false
+		self.lockingThread = nil
+	end
+end
+
+function Mutex:isLocked()
+	return self.locked
+end
+
+return Mutex

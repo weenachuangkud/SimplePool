b10fc9acd626d50c4086bc13f945bc988d7530ad
Mawin <weena.chuangkud@gmail.com>
2025-10-07 19:41:59 +0700

    Update SimplePool.lua

--- src/SimplePool.lua
+++ src/SimplePool.lua
@@ -94,7 +94,7 @@ function SimplePool:Prewarm(amount: number)
 end
 
 function SimplePool:Clear()
-	for _, v in self.Pool do
+	for _, v : Instance in self.Pool do
 		v:Destroy()
 	end
 	self.Pool = {}
